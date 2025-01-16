3d52ce68172f328f5300a7420501705d56705df5
Christian Sachs <byaskew3@gmail.com>
2025-01-16 08:12:39 -0700

    E Commerce Build Part 2 Starter Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import { QueryClient, QueryClientProvider } from "react-query"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./components/Home"
-import { Provider } from "react-redux"
-import store from "./redux/store"
-import ShoppingCart from "./components/ShoppingCart"
+
 
 const queryClient = new QueryClient()
 
 const App = () => {
   return (
-    <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home/>}/>
-            <Route path="/cart" element={<ShoppingCart/>}/>
           </Routes>
         </BrowserRouter>
       </QueryClientProvider>
-    </Provider>
   )
 }
 export default App
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,6 @@ import { fetchCategories, fetchProducts } from "../api/api"
 import { useState } from "react"
 import { Product } from "../types/types"
 import ProductCard from "./ProductCard"
-import { Link } from "react-router-dom"
 
 const Home = () => {
     const { data: products } = useQuery({
@@ -35,7 +34,6 @@ const Home = () => {
                 <option key={category} value={category}>{category}</option>
             ))}
         </select>
-        <Link to={'/cart'}>Cart</Link>
         <div>
             {filteredProducts?.map((product: Product) => (
                 <ProductCard key={product.id} product={product}/>
